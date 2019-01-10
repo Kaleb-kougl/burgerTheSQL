f95383b2124f5d42779f3011e4f3f76d676658de
Kaleb Kougl <kalebkougl@gmail.com>
2019-01-09 22:03:04 -0600

    connection to burger db, necessary packages.

--- migrations/sequelize.js
+++ migrations/sequelize.js
@@ -1,13 +1,24 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('burgers_db_sequelize', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql',
+  "username": "root",
+  "password": "root",
+  "database": "burgers_db",
+  "host": "localhost",
+  "port": "8889",
+  "dialect": "mysql"
+}
+);
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  operatorsAliases: false
+const Burger = sequelize.define('burger', {
+  burger_name: Sequelize.STRING,
+  devoured: Sequelize.BOOLEAN
 });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
